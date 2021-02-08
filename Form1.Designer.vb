3cd7762f3e4df0270c64d1a18cf0e2a41445736b
cole1234 <30983090+cole1234@users.noreply.github.com>
2021-02-07 20:48:14 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txbAdress = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.tmrStatus = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txbAdress
+        '
+        Me.txbAdress.AccessibleName = "Adress"
+        Me.txbAdress.Location = New System.Drawing.Point(80, 12)
+        Me.txbAdress.Name = "txbAdress"
+        Me.txbAdress.Size = New System.Drawing.Size(553, 20)
+        Me.txbAdress.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 7)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(62, 28)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Go!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(6, 38)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(966, 502)
+        Me.WebBrowser1.TabIndex = 2
+        '
+        'tmrStatus
+        '
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(6, 543)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(978, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(639, 8)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 24)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Back!"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(742, 10)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(111, 21)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Forward!"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(871, 7)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(101, 24)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Refresh!"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(984, 561)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txbAdress)
+        Me.MinimumSize = New System.Drawing.Size(1000, 600)
+        Me.Name = "Form1"
+        Me.Text = "VBBrowse"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txbAdress As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents tmrStatus As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser1.ScriptErrorsSuppressed = True
+    End Sub
+
+    Sub checkAdress() Handles WebBrowser1.DocumentCompleted
+        txbAdress.Text = WebBrowser1.Url.ToString
+    End Sub
+
+    Private Sub tmrStatus_Tick(sender As Object, e As EventArgs) Handles tmrStatus.Tick
+        Label1.Text = WebBrowser1.StatusText.ToString
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If WebBrowser1.CanGoBack Then
+            WebBrowser1.GoBack()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If WebBrowser1.CanGoBack Then
+            WebBrowser1.GoForward()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        WebBrowser1.Refresh()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebBrowser1.Navigate(txbAdress.Text)
+    End Sub
+End Class
